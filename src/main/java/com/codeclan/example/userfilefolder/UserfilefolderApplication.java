a4eb59fdcbdb78544b70c6f2ae8f7afbe7d5a98f
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-21 20:24:26 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/userfilefolder/UserfilefolderApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.userfilefolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserfilefolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserfilefolderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/userfilefolder/UserfilefolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.userfilefolder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserfilefolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
